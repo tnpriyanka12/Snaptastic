b354c0cd483ceff3637cc01b966068a52f877997
tnpriyanka12 <tnpriyanka12@gmail.com>
2018-03-07 17:19:28 +1100

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.serve_static_files = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
